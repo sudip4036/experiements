497fdc17cf4e53b162dcaa1913621b623af0fa77
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 18:26:13 +0530

    Production class defined for Rock Beats Scissor test.

---
+++ src/main/java/com/sapient/execise/tdd/rps/Move.java
@@ -0,0 +1,19 @@
+package com.sapient.execise.tdd.rps;
+
+public class Move {
+	
+	private String move;
+	
+	public Move(String move) {
+		this.move = move;
+	}
+
+	public boolean beats(String anotherMove) {
+		switch(move){
+		case "ROCK":
+            return anotherMove == "SCISSORS";
+		}
+		return false;
+	}
+
+}
