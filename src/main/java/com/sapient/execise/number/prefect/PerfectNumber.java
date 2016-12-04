f3b69ffcbbc7c33b755ac52377545de94cdd6f68
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 16:32:19 +0530

    Success test for all valid Perfect Number scenarios

--- src/main/java/com/sapient/execise/number/prefect/PerfectNumber.java
+++ src/main/java/com/sapient/execise/number/prefect/PerfectNumber.java
@@ -1,9 +1,29 @@
 package com.sapient.execise.number.prefect;
 
+
+/**
+ * Class which identifies Perfect Number
+ * 
+ * @author sdas14
+ *
+ */
 public class PerfectNumber {
 
-	public boolean isValidPerfectNumber(int i) {
-		if(i == 6)
+	/**
+	 * This method identifies if it a number is perfect number
+	 * @param number
+	 * @return true if number is perfect, otherwise false
+	 */
+	public boolean isValidPerfectNumber(int number) {
+		int sum = 1;
+        //Calculate Sum for all its divisors
+		for (int divisors = 2; divisors <= number / 2; divisors++) {
+            if (number % divisors == 0) {
+                sum += divisors;
+            }
+        }
+		//Verify if sum of all its divisors is equal to the number
+        if(number == sum)
 			return true;
 		
 			return false;
