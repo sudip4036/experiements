975be63d6bf6ece18f5d00b37e515bd87bcc58ef
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 22:28:38 +0530

    As string comperison is expensieve,moving to enum,compilation error

--- src/test/java/com/sapient/execise/tdd/rps/RPSMoveTest.java
+++ src/test/java/com/sapient/execise/tdd/rps/RPSMoveTest.java
@@ -10,8 +10,8 @@ public class RPSMoveTest {
 
 	   @Test
 	   public void testRocksBeatsScissors() throws Exception {
-		   Move move = new Move("ROCK");
-		   boolean isWinner = move.beats("SCISSORS");
+		   Move move = Move.valueOf("ROCK");
+		   boolean isWinner = move.beats(Move.valueOf("SCISSORS"));
 		   assertEquals(isWinner, true);
 		   
 	   }
@@ -26,24 +26,24 @@ public class RPSMoveTest {
 	   
 	   @Test
 	   public void testScissorsBeatsPaper() throws Exception {
-		   Move move = new Move("SCISSORS");
-		   boolean isWinner = move.beats("PAPER");
+		   Move move = Move.valueOf("SCISSORS");
+		   boolean isWinner = move.beats(Move.valueOf("PAPER"));
 		   assertEquals(isWinner, true);
 		   
 	   }
 	   
 	   @Test
 	   public void testInvalidInput() throws Exception {
-		   Move move = new Move("ABC");
-		   boolean isWinner = move.beats("XYZ");
+		   Move move = Move.valueOf("ABC");
+		   boolean isWinner = move.beats(Move.valueOf("XYZ"));
 		   assertEquals(isWinner, false);
 		   
 	   }
 	   
 	   @Test
 	   public void testTieInput() throws Exception {
-		   Move move = new Move("PAPER");
-		   boolean isWinner = move.beats("PAPER");
+		   Move move = Move.valueOf("PAPER");
+		   boolean isWinner = move.beats(Move.valueOf("PAPER"));
 		   assertEquals(isWinner, false);
 		   
 	   }
