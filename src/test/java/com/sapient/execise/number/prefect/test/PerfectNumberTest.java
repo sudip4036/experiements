e8cad7cd9b5679a47b186349797fb111cfa6345c
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 16:16:08 +0530

    Test with multiple Perfect number

--- src/test/java/com/sapient/execise/number/prefect/test/PerfectNumberTest.java
+++ src/test/java/com/sapient/execise/number/prefect/test/PerfectNumberTest.java
@@ -2,16 +2,33 @@ package com.sapient.execise.number.prefect.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.sapient.execise.number.prefect.PerfectNumber;
+import com.sapient.execise.rps.game.Move;
 
+@RunWith(Parameterized.class)
 public class PerfectNumberTest {
 	
+	@Parameters
+	   public static Iterable<Object[]> testData() {
+	    	return Arrays.asList(new Object[][] {
+	             {6}, {28}, {496}, {8128}, });
+	    }
+	   
+ @Parameter(0)
+ public int     number;
+ 
 	@Test
 	public void testPerfectNumber() throws Exception {
 		PerfectNumber perfectNumber = new PerfectNumber();
-		boolean isPerfect = perfectNumber.isValidPerfectNumber(6);
+		boolean isPerfect = perfectNumber.isValidPerfectNumber(number);
 		assertEquals(isPerfect,true);
 	}
 	
