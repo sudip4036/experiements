696eb84f3b8cfbd11491b3d5d7bcd3b31b46a0d8
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 15:48:46 +0530

    Perfect Number, first case

---
+++ src/test/java/com/sapient/execise/number/prefect/PerfectNumber.java
@@ -0,0 +1,10 @@
+package com.sapient.execise.number.prefect;
+
+public class PerfectNumber {
+
+	public boolean isValidPerfectNumber(int i) {
+		return true;
+				
+	}
+
+}
---
+++ src/test/java/com/sapient/execise/number/prefect/test/PerfectNumberTest.java
@@ -0,0 +1,18 @@
+package com.sapient.execise.number.prefect.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.sapient.execise.number.prefect.PerfectNumber;
+
+public class PerfectNumberTest {
+	
+	@Test
+	public void testPerfectNumber() throws Exception {
+		PerfectNumber perfectNumber = new PerfectNumber();
+		boolean isPerfect = perfectNumber.isValidPerfectNumber(6);
+		assertEquals(isPerfect,true);
+	}
+
+}
