91db96b4e46011c8f19c6c55bf6b8488832aa837
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 23:45:38 +0530

    Updated test class to address all scenario of Player A win

--- src/test/java/com/sapient/execise/tdd/rps/RoundTest.java
+++ src/test/java/com/sapient/execise/tdd/rps/RoundTest.java
@@ -2,22 +2,47 @@ package com.sapient.execise.tdd.rps;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
 
 /**
  * This test class helps to identify the winner of a game based on the move
  * @author sdas14
  *
  */
+
+@RunWith(Parameterized.class)
 public class RoundTest {
+
 	
-	   @Test
-	   public void testPlayerAWinner() throws Exception {
-		   Move moveByPlayerA = Move.SCISSORS;
-		   Move moveByPlayerB = Move.PAPER;
-		   Round round = new Round(moveByPlayerA, moveByPlayerB);
-		   String player = round.getWinner();
-		   assertEquals(player, "A");
+	   @Parameters
+	   public static Iterable<Object[]> testData() {
+	    	return Arrays.asList(new Object[][] {
+	             { Move.ROCK, Move.SCISSORS, "A" },{ Move.PAPER, Move.ROCK, "A" }, 
+	             { Move.SCISSORS, Move.PAPER, "A" },});
 	   }
+	   
+	   @Parameter(0)
+	    public Move     moveA;
+
+	    @Parameter(1)
+	    public Move     moveB;
+
+	    @Parameter(2)
+	    public String   expectedResult;
+	    	
+	   @Test
+	   public void testGetWinner() throws Exception {
+		   Round round = new Round(moveA, moveB);
+		   String player = round.getWinner();
+		   assertEquals(player, expectedResult);
+	   }
+
 
 }
