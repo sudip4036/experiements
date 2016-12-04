e046167e5f9e4ee13d4959b631d1748b4e645bad
sdas14 <sdas14@CHIsdas1462192.sapient.com>
2016-12-04 23:23:35 +0530

    Main class defined to identify the winner, still failed state

---
+++ src/main/java/com/sapient/execise/tdd/rps/Round.java
@@ -0,0 +1,30 @@
+package com.sapient.execise.tdd.rps;
+
+import com.sapient.execise.tdd.rps.Move;
+
+/**
+ * This class identify the winner of a game based on the move by two player
+ * @author sdas14
+ *
+ */
+public class Round {
+
+    private final Move moveByPlayerA;
+    private final Move moveByPlayerB;
+    
+	public Round(Move moveByPlayerA, Move moveByPlayerB) {
+		this.moveByPlayerA = moveByPlayerA;
+		this.moveByPlayerB = moveByPlayerB;
+	}
+
+	/**
+	 * This method identifies who is the winner.
+	 * 
+	 * @return Player if there is winner
+	 */
+	public String getWinner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
